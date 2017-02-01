29fb707d7a30ebec4ff1a06e661f8ec2d67aedf1
Lorna Jane Mitchell <lorna@lornajane.net>
2017-02-01 16:35:22 +0000

    Remove insecure switches now cert problem is fixed

--- log-me.sh
+++ log-me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wsk activation list -i -l1 | tail -n1 | cut -d ' ' -f1 | xargs wsk activation -i logs
+wsk activation list -l1 | tail -n1 | cut -d ' ' -f1 | xargs wsk activation logs
 # wsk activation list -l1 | grep "^[0-9a-f]\{32\}" | cut -c1-32 | xargs wsk activation logs
